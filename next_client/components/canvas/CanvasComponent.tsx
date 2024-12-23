51f6efbac7956c1b0df9dba401c700db83e01e6e
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-12-24 00:45:48 +0530

    v1.0 of fade out pen

--- next_client/components/canvas/CanvasComponent.tsx
+++ next_client/components/canvas/CanvasComponent.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
-import { X, PencilLine, ALargeSmall, Eraser } from "lucide-react";
+import { X, PencilLine, ALargeSmall, Eraser, PencilOff } from "lucide-react";
 import { Button } from "../ui/button";
+import { sleep } from "../misc/fadeout";
 
 type CanvasState = {
   drawings: ({ x: number; y: number; isEraser?: boolean } | undefined)[][];
   offset: { x: number; y: number };
   scale: number;
+};
+
+type TempCanvasState = {
+  drawings: ({ x: number; y: number } | undefined)[][];
+  opacity: number;
 };
 
 export default function CanvasComponent({
@@ -27,6 +33,11 @@ export default function CanvasComponent({
   });
   const [lastPosition, setLastPosition] = useState({ x: 0, y: 0 });
   const [activeBtn, setActiveBtn] = useState("draw");
+  const [isTempDrawing, setIsTempDrawing] = useState(false);
+  const [tempCanvasState, setTempCanvasState] = useState<TempCanvasState>({
+    drawings: [],
+    opacity: 1,
+  });
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -59,6 +70,7 @@ export default function CanvasComponent({
     context.translate(canvasState.offset.x, canvasState.offset.y);
     context.scale(canvasState.scale, canvasState.scale);
 
+    // draw the permanent drawings
     canvasState.drawings.forEach((drawing) => {
       if (!drawing[0]) return;
       context.beginPath();
@@ -77,7 +89,27 @@ export default function CanvasComponent({
     });
 
     context.restore();
-  }, [canvasState]);
+
+    context.save();
+
+    //draw the temporary drawings
+    context.globalAlpha = tempCanvasState.opacity;
+    tempCanvasState.drawings.forEach((drawing) => {
+      if (!drawing[0]) return;
+      context.beginPath();
+      context.moveTo(drawing[0].x, drawing[0].y);
+      drawing.forEach((point) => {
+        if (!point) return;
+        context.lineTo(point.x, point.y);
+      });
+      context.lineCap = "round";
+      context.strokeStyle = "red";
+      context.fillStyle = "red";
+      context.lineWidth = 4;
+      context.stroke();
+    });
+    // context.restore();
+  }, [canvasState, tempCanvasState]);
 
   const getCoordinates = (e: React.MouseEvent) => {
     const canvas = canvasRef.current;
@@ -100,6 +132,15 @@ export default function CanvasComponent({
     const coords = getCoordinates(e);
     if (!coords) return;
     if (e.button === 0) {
+      if (activeBtn === "disPencil") {
+        console.log("Dissapear pencil");
+        setIsTempDrawing(true);
+        setTempCanvasState((prev) => ({
+          ...prev,
+          drawings: [[{ ...coords }]],
+        }));
+        return;
+      }
       if (activeBtn === "draw" || activeBtn === "erase") {
         setIsDrawing(true);
         setCanvasState((prev) => ({
@@ -124,6 +165,16 @@ export default function CanvasComponent({
   };
 
   const handleMouseMove = (e: React.MouseEvent) => {
+    if (isTempDrawing) {
+      const coords = getCoordinates(e);
+      if (!coords) return;
+      setTempCanvasState((prev) => {
+        const updatedDrawings = [...prev.drawings];
+        const lastDrawing = updatedDrawings[updatedDrawings.length - 1];
+        lastDrawing.push({ ...coords });
+        return { ...prev, drawings: updatedDrawings };
+      });
+    }
     if (isDrawing) {
       const coords = getCoordinates(e);
       if (!coords) return;
@@ -148,15 +199,48 @@ export default function CanvasComponent({
     }
   };
 
-  const handleMouseUp = () => {
+  const fadeOut = async () => {
+    let fadeTime = 100;
+    let fadeOutValue = 0.05;
+
+    return new Promise((resolve) => {
+      const fadeEffect = setInterval(() => {
+        setTempCanvasState((prev) => {
+          if (prev.opacity <= 0) {
+            clearInterval(fadeEffect);
+            return { drawings: [], opacity: 1 };
+          }
+          return { ...prev, opacity: prev.opacity - fadeOutValue };
+        });
+      }, fadeTime);
+    });
+    // while (tempCanvasState.opacity > 0) {
+    //   console.log(tempCanvasState.opacity);
+    //   // console.log("downing the opacity");
+    //   setTempCanvasState((prev) => ({
+    //     ...prev,
+    //     opacity: prev.opacity - fadeOutValue,
+    //   }));
+    //   await sleep(fadeTime);
+    // }
+    // console.log("temp Canvas state", tempCanvasState.drawings);
+    // if (tempCanvasState.opacity <= 0) {
+    //   setTempCanvasState((prev) => ({ ...prev, drawings: [] }));
+    // }
+  };
+
+  const handleMouseUp = async () => {
     console.log(canvasState.drawings);
     const canvas = canvasRef.current;
     if (!canvas) return;
+    const context = canvas.getContext("2d");
+    if (!context) return;
 
     setIsDrawing(false);
     setIsPanning(false);
+    setIsTempDrawing(false);
+    await fadeOut();
   };
-
   const handleWheel = (e: React.WheelEvent) => {
     const canvas = canvasRef.current;
     if (!canvas) return;
@@ -224,6 +308,14 @@ export default function CanvasComponent({
           size={"icon"}
         >
           <Eraser />
+        </Button>
+        <Button
+          id="disPencil"
+          variant={activeBtn === "disPencil" ? "default" : "ghost"}
+          size={"icon"}
+          onClick={handleBtnClick}
+        >
+          <PencilOff />
         </Button>
       </div>
     </div>
